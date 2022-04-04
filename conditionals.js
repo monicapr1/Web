98616f8090ef3831cce3ef96c252823783340005
monicapr1 <monicapr858@gmail.com>
2022-04-04 12:37:45 -0500

    segundo

---
+++ conditionals.js
@@ -0,0 +1,22 @@
+console.log("conditionals")
+
+var n1=10
+var n2=15
+
+if (n1==n2){
+    console.log("los numeros son iguales")
+}else if(n1> n2){
+    console.log(n2 +" es el mayor" )
+}else{
+    console.log(n2+" es el mayor")
+}
+var bandera=true
+if (bandera) {
+    console.log("la condicion se cumple")
+}else{
+    console.log("La condicion no se cumple")
+    document.write("no se cumple")
+}
+
+document.write("contenido js enviado a html")
+
